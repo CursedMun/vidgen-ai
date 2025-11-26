02dc77235e63199078c0246514d266296a88758b
cursedmun <frosty.hyperx@gmail.com>
2025-11-26 18:06:21 +0000

    Feat: Implement shadcn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
   import favicon from "$lib/assets/favicon.svg"
   import "../app.css"
-
+  import { ModeWatcher } from "mode-watcher"
   const { children } = $props()
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon}>
 </svelte:head>
-
-<div class="min-h-screen flex items-center justify-center p-6">
-  <div class="w-full max-w-2xl">{@render children()}</div>
-</div>
+<ModeWatcher/>
+{@render children?.()}
